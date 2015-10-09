d83f188dfbe80382f191fddca311ad3be87125ee
Maxime Dhalluin <maxime.dhalluin@gmail.com>
2015-10-09 17:55:00 +0200

    Resolution of AOP proxying error

--- src/main/java/com/melody/security/JavaMelodyConfiguration.java
+++ src/main/java/com/melody/security/JavaMelodyConfiguration.java
@@ -9,6 +9,8 @@ import org.springframework.boot.context.embedded.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 
 import net.bull.javamelody.MonitoringFilter;
 import net.bull.javamelody.Parameter;
@@ -17,8 +19,10 @@ import net.bull.javamelody.SessionListener;
 /**
  * @author speralta, evernat
  */
+
 @Configuration
-@ImportResource("classpath:net/bull/javamelody/monitoring-spring.xml")
+//@ImportResource("classpath:net/bull/javamelody/monitoring-spring.xml")
+@ImportResource("classpath:net/bull/javamelody/monitoring-spring-aspectj.xml")
 public class JavaMelodyConfiguration implements ServletContextInitializer {
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
@@ -34,6 +38,8 @@ public class JavaMelodyConfiguration implements ServletContextInitializer {
 		// see the list of parameters:
 		// https://github.com/javamelody/javamelody/wiki/UserGuide#6-optional-parameters
 		javaMelody.addInitParameter(Parameter.LOG.getCode(), Boolean.toString(true));
+		
+		
 		// to add basic auth:
 		// javaMelody.addInitParameter(Parameter.AUTHORIZED_USERS.getCode(), "admin:pwd");
 		// to change the default storage directory:
